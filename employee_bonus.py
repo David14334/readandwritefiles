760e9a86006f055dd6e179a7022f8a95e604c14d
David Rangel <daron1155@gmail.com>
2022-02-02 16:04:41 -0600

    R1

--- employee_bonus.py
+++ employee_bonus.py
@@ -11,11 +11,11 @@ total_pay= 0
 
 
 for record in employee_file:
-        bonus= float(record[3]) * float(record[4])
+        bonus = float(record[3]) * float(record[4])
         total_pay = float(record[3]) * bonus
 
         print("FName:", record[1])
         print("LName:", record[2])
         print("Salary:", "${:,.2f}".format(float(record[3]))
-        print("Bonus:", "${:,.2f}".format(bonus)
-        print("Total Pay:","${:,.2f}".format(total_pay)
+        print("Bonus:", "${:,.2f}".format(bonus))
+        print("Total Pay:","${:,.2f}".format(total_pay))
